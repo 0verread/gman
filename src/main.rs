61a85e2a1f6d901f71123ec3ca361c8b45ea811b
sub0ne1 <iamsubhajit.d@gmail.com>
2024-05-13 15:13:02 -0400

    Working code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use clap::Parser;
-use rand::prelude::*;
+use rand::thread_rng;
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
-
+use rand::seq::SliceRandom;
 
 #[derive(Parser)]
 struct Cli{
@@ -14,19 +14,20 @@ struct Cli{
 
 fn build_mc(words: &[String]) -> HashMap<String, Vec<String>> {
     let mut mc = HashMap::new();
-
-    for window in words.windows(2){
-        let (prefix, suffix) = (window[0], window[1]);
-        mc.entry(prefix.to_string()).or_insert_with(Vec::new).push(suffix.to_string());
+    for word in words {
+        for (i, window) in word.chars().collect::<Vec<_>>().windows(2).enumerate() {
+            let prefix = window.iter().collect::<String>();
+            let suffix = word.chars().skip(i + 2).collect::<String>();
+            mc.entry(prefix).or_insert_with(Vec::new).push(suffix);
+        }
     }
-
     return mc;
 }
 
 fn generate_name(mc: &HashMap<String, Vec<String>>, max_len: usize) -> String {
     let mut rng = thread_rng();
-    let mut state = mc.keys().choose(&mut rng)
-                    .unwrap().to_string();
+    let mut keys: Vec<_> = mc.keys().cloned().collect();
+    let mut state = keys.choose(&mut rng).unwrap().to_string();
     let mut name = state.clone();
 
     while name.len() < max_len {
@@ -38,23 +39,29 @@ fn generate_name(mc: &HashMap<String, Vec<String>>, max_len: usize) -> String {
     return name;
 }
 
+fn read_words_from_file(filename: &str) -> Result<Vec<String>, std::io::Error> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+    let mut words = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let mut word_iter = line.split_whitespace();
+
+        while let Some(word) = word_iter.next() {
+            words.push(word.to_string());
+        }
+    }
+
+    Ok(words)
+}
 
 fn main() {
     let args = Cli::parse();
     let file_path = args.file.to_str().unwrap();
-    let file = File::open(file_path).expect("Failed to open this file");
-    let reader = BufReader::new(file);
-    let mut words = Vec::new();
-   
-    for line in reader.lines() {
-        let word = line;
-        words.push(word);
-    }
-
-    let mc  = build_mc(&words);
-    for _ in 0..10 {
-        let name = generate_name(&mc, 10);
-        println!("{}", name);
-    }
+    let words = read_words_from_file(file_path).expect("Failed to read words from file");
+    let mc = build_mc(&words);
+    let name = generate_name(&mc, 3);
+    println!("Name: {:?}", name);
 }
 
